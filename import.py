49b3f3082fc3079df32b993ef990fc3e04fd3c8c
turbopok <30575544+turbopok@users.noreply.github.com>
2019-01-23 16:10:19 +0800

    Create import.py

---
+++ import.py
@@ -0,0 +1,10 @@
+# Import Modules
+from requests import get
+from bs4 import BeautifulSoup
+from time import sleep
+from time import time
+import random
+import sqlite3
+import re
+from datetime import datetime
+import datetime as dt
